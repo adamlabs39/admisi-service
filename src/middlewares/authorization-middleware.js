3c6c55cc4d381592ad0c926c97a21d8b2f89bf01
alliano <allianoanonymous@gmail.com>
2024-12-17 14:39:04 +0700

    update authorization sdk dan ajust

--- src/middlewares/authorization-middleware.js
+++ src/middlewares/authorization-middleware.js
@@ -1,12 +1,13 @@
 import JwtHelper from "../helper/jwt-helper.js";
 import { Context as Ctx } from "./context.js";
 import { CTX_AUTHOR } from "../constant/context-constant.js";
+import { JwtUtils } from "@adameds/authorization-sdk/jwt-utils"
 const authorizationMiddleware = async (request, response, nextFunction) => {
     try {
         const BEARER_TOKEN = request.get("Authorization");
         if (!BEARER_TOKEN) return response.status(401).json({message: `silakan login terlebih dahulu!`});
         const token = BEARER_TOKEN.split(" ")[1]; 
-        const isValid = await JwtHelper.verify(token);
+        const isValid = JwtUtils.veryfy(token);
         if (!isValid) return response.status(401).json({message: `token tidak valid!`});
         response.locals.jwtData = isValid;
         console.log("isValid", isValid);
