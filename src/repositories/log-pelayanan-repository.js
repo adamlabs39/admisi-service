b3d13006b9cad92a10ccb7b8c9f9b72d93a7ad35
hafisrabbani <hafis.gov@gmail.com>
2024-12-23 09:58:38 +0700

    fix

--- src/repositories/log-pelayanan-repository.js
+++ src/repositories/log-pelayanan-repository.js
@@ -216,7 +216,7 @@ export default class LogPelayananRepository {
                                 as: "pegawai",
                                 required: true,
                                 where: {deletedAt: {[Op.is]: null}},
-                                attributes: ["title", ["name","nama"], "gender"]
+                                attributes: ["first_title", "last_title", ["name","nama"], "gender"]
                             }
                         ]
                     },
@@ -454,7 +454,7 @@ export default class LogPelayananRepository {
                                 as: "pegawai",
                                 required: true,
                                 where: {deletedAt: {[Op.is]: null}},
-                                attributes: ["title", ["name", "nama"], "gender"]
+                                attributes: ["first_title", "last_title", ["name", "nama"], "gender"]
                             }
                         ]
                     },
