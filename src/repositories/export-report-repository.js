a992dc03c43b7de4d030a7b10cc5d0ce630aa449
Raihan <raihanm854@gmail.com>
2025-08-20 10:08:56 +0700

    feat: menambah filter untuk export report kunjungan

--- src/repositories/export-report-repository.js
+++ src/repositories/export-report-repository.js
@@ -6,16 +6,27 @@ import { InsuranceAccountModel } from "@adameds/model-sdk/admisi";
 import { Context } from "../middlewares/context.js";
 import { CTX_AUTHOR } from "../constant/context-constant.js";
 import { Op } from "sequelize";
+import sequelizeInstance from "../configurations/sequelize-instance.js";
 
 export default class ExportReportRepository {
-    static async getExportKunjungan() {
+    static async getExportKunjungan(args) {
         const { faskesUuid } = Context.get(CTX_AUTHOR);
         return await LogPelayananModel.findAll({
             where: {
-                faskesUuid,
-                deletedAt: {
-                    [Op.is]: null
-                }
+            faskesUuid,
+            deletedAt: {    [Op.is]: null,  },
+            [Op.or]: [
+            { noreg: { [Op.iLike]: `%${args.q || ""}%` } }, // Find by no_rm
+            sequelizeInstance.where(sequelizeInstance.fn("concat", sequelizeInstance.col("patient.title"), " ", sequelizeInstance.col("patient.name")), { [Op.iLike]: `%${args.q || ""}%` }), // Find by title and name
+            sequelizeInstance.where(sequelizeInstance.col("patient.address.full_address"), { [Op.iLike]: `%${args.q || ""}%` }), // Find by address
+            sequelizeInstance.where(sequelizeInstance.col("patient.no_rm"), { [Op.iLike]: `%${args.q || ""}%` }), // Find By Rm patient
+            ],
+            tglRegistrasi: {
+                [Op.between]: [args.start_date, args.end_date],
+            },
+            jenisKunjungan: {
+                [Op.eq]: `${args.jenis_kunjungan}`
+            },
             },
             include: [
                 {
@@ -95,5 +106,89 @@ export default class ExportReportRepository {
             attributes: ["tgl_registrasi", "noreg", "no_pelayanan", "jenis_kunjungan", "patient_uuid"]
         });
     }
+
+    // static async getExportBatalKunjungan() {
+    //     const { faskesUuid } = Context.get(CTX_AUTHOR);
+    //     return await LogPelayananModel.findAll({
+    //         where: {
+    //             faskesUuid,
+    //             deletedAt: {
+    //                 [Op.is]: null
+    //             }
+    //         },
+    //         include: [
+    //             {
+    //                 model: PatientModel,
+    //                 as: "patient",
+    //                 required: true,
+    //                 where: {
+    //                     deletedAt: {
+    //                         [Op.is]: null
+    //                     }
+    //                 },
+    //                 include: [
+    //                     {
+    //                         model: AddressModel,
+    //                         as: "address",
+    //                         required: true,
+    //                         where: {
+    //                             deletedAt: {
+    //                                 [Op.is]: null
+    //                             }
+    //                         },
+    //                         attributes: ["prov", "city", "district", "rt", "rw", "full_address", "country", "village"],
+    //                     },
+    //                     {
+    //                         model: BirthDetailModel,
+    //                         as: "birth_detail",
+    //                         required: true,
+    //                         where: {
+    //                             deletedAt: {
+    //                                 [Op.is]: null
+    //                             }
+    //                         },
+    //                         attributes: ["age_year", "age_month", "age_day", "birth_date"],
+    //                     },
+    //                 ],
+    //                 attributes: ["uuid", "title", "name", "identity", "no_identity", "phone", "gender", "no_rm"],
+    //             },
+    //             {
+    //                 model: PractitionerModel,
+    //                 as: "practitioner",
+    //                 required: true,
+    //                 where: {
+    //                     deletedAt: {
+    //                         [Op.is]: null
+    //                     }
+    //                 },
+    //                 attributes: ["uuid"],
+    //                 include: [
+    //                     {
+    //                         model: PegawaiModel,
+    //                         as: "pegawai",
+    //                         required: true,
+    //                         where: {
+    //                             deletedAt: {
+    //                                 [Op.is]: null
+    //                             }
+    //                         },
+    //                         attributes: ["first_title", "last_title", ["name", "nama"], "gender"]
+    //                     }
+    //                 ],
+    //             },
+    //             {
+    //                 model: LokasiModel,
+    //                 as: "lokasi",
+    //                 required: false,
+    //                 where: { deletedAt: { [Op.is]: null } },
+    //                 attributes: ["name"], 
+    //             },
+    //         ],
+    //         attributes: ["tgl_registrasi", "noreg", "no_pelayanan", "jenis_kunjungan", "patient_uuid", "lokasi_uuid", "cancel_reason", "cancel_date", "cancel_by"],
+    //         where: {
+    //             status: false
+    //         }
+    //     });
+    // }
 
 }
--- src/services/export-report-service.js
+++ src/services/export-report-service.js
@@ -1,9 +1,15 @@
 import ExportReportRepository from "../repositories/export-report-repository.js";
 
 export default class ExportReportService {
-    static async getExportKunjungan(){
-        const result = await ExportReportRepository.getExportKunjungan();
+    static async getExportKunjungan(args){
+        const result = await ExportReportRepository.getExportKunjungan(args);
         if(!result) throw new Error("Gagal mengambil export kunjungan");
+        return result;
+    }
+    
+    static async getExportBatalKunjungan(){
+        const result = await ExportReportRepository.getExportBatalKunjungan();
+        if(!result) throw new Error("Gagal mengambil export batal kunjungan");
         return result;
     }
 }
